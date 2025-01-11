8a1454cacbd632011bef3ee3f70175be7dc6eea7
Nuno Ribeiro <a104177@alunos.uminho.pt>
2025-01-11 18:45:02 +0000

    updates

--- assets/js/stepsSpeedy.js
+++ assets/js/stepsSpeedy.js
@@ -1,6 +1,19 @@
 // Seleciona todos os elementos de step
 const steps = document.querySelectorAll('.step');
 
+// Seleciona o elemento de imagem
+const stepImage = document.getElementById('step-image');
+
+// Imagens para cada passo
+const stepImages = {
+  1: "../assets/images/SScooter1_2_3.png",
+  2: "../assets/images/SScooter1_2_3.png",
+  3: "../assets/images/SScooter1_2_3.png",
+  4: "../assets/images/SScooter4.png",
+  5: "../assets/images/SScooter5.png",
+  6: "../assets/images/SpeedyScooter.png"
+};
+
 // Adiciona um evento de clique para cada step
 steps.forEach(step => {
   step.addEventListener('click', () => {
@@ -9,5 +22,11 @@ steps.forEach(step => {
     
     // Adiciona a classe 'active' ao step clicado
     step.classList.add('active');
+    
+    // Pega o número do passo
+    const stepNumber = step.getAttribute('data-step');
+    
+    // Atualiza a imagem com base no passo
+    stepImage.src = stepImages[stepNumber];
   });
 });
