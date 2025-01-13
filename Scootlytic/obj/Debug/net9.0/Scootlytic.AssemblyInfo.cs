cb12074114a87ba663c4237ec6c51b6c7477ebe0
Nuno Ribeiro <a104177@alunos.uminho.pt>
2025-01-13 11:37:54 +0000

    ya qualquer cena

--- Scootlytic/obj/Debug/net9.0/Scootlytic.AssemblyInfo.cs
+++ Scootlytic/obj/Debug/net9.0/Scootlytic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scootlytic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d3b6aeade24ef704cb4f1b2551f347f6de7b80b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070193f46b95fa807e1e3fa4266fddebe610d2c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scootlytic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scootlytic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
